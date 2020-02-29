f44ac4639f3e1a149ece4ef795efdd0ff9802101
Gabriel Rosenbach <gabrielrosenbach@hotmail.com>
2020-02-29 19:08:27 -0300

    Saving Seller

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -149,13 +151,36 @@ public class SellerFormController implements Initializable {
 
 			exception.addError("name", "Field can't be empty");
 		}
-		seller.setName(txtName.getText());
+
+		if (txtEmail.getText() == null || txtEmail.getText().trim().isEmpty()) {
+
+			exception.addError("email", "Field can't be empty");
+		}
+
+		if (dpBirthDate.getValue() == null) {
+
+			exception.addError("birthDate", "Field can't be empty");
+		}
+
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().isEmpty()) {
+
+			exception.addError("baseSalary", "Field can't be empty");
+		}
 
 		if (exception.getErrors().size() > 0) {
 
 			throw exception;
 		}
-
+		
+		seller.setName(txtName.getText());
+		seller.setEmail(txtEmail.getText());
+		
+		Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault()));
+		seller.setBirthDate(Date.from(instant));
+		
+		seller.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+		seller.setDepartment(comboBoxDepartment.getValue());
+		
 		return seller;
 	}
 
@@ -235,10 +260,13 @@ public class SellerFormController implements Initializable {
 
 		Set<String> fields = errors.keySet();
 
-		if (fields.contains("name")) {
+		labelErrorName.setText(fields.contains("name") ? errors.get("name") : "");
 
-			labelErrorName.setText(errors.get("name"));
-		}
+		labelErrorEmail.setText(fields.contains("email") ? errors.get("email") : "");
+
+		labelErrorBirthDate.setText(fields.contains("birthDate") ? errors.get("birthDate") : "");
+
+		labelErrorBaseSalary.setText(fields.contains("baseSalary") ? errors.get("baseSalary") : "");
 	}
 
 }
